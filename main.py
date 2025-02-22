a4a23befc1cc07c70ffdee247a5bf68122421619
atrox3d <atrox3d@gmail.com>
2025-02-22 08:47:06 +0100

    adding logger.trace, level=15

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ app.command('db'  )(populate_database.main)
 app.command('query'  )(query_data.main)
 
 
+MY_DEBUG_LEVEL = 15
+logging.addLevelName(MY_DEBUG_LEVEL, "trace")
+
+
+# 2. Add the custom level to the Logger class
+def trace(self, message, *args, **kws):
+    """Logs a message with level 'MYDEBUG' on this logger.
+
+    The arguments are interpreted as for debug().
+    """
+    if self.isEnabledFor(MY_DEBUG_LEVEL):
+        self._log(MY_DEBUG_LEVEL, message, args, **kws)
+
+logging.Logger.trace = trace
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=MY_DEBUG_LEVEL)
+    logger.debug('test debug')
+    logger.trace('test trace')
     app()
